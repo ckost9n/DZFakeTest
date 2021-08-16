5239f939b98a364e841c7d526370be7553d0b85f
ckost9n <ckost9n@gmail.com>
2021-08-16 18:17:49 +0300

    setup autolayout

---
+++ DZFakeTest/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  DZFakeTest
+//
+//  Created by Konstantin on 15.08.2021.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func cancelTapped(_ sender: UIButton) {
+    }
+    
+}
--- DZFakeTest/ViewController.swift
+++ DZFakeTest/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    @IBAction func loginTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func helpUserTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func helpPasswordTapped(_ sender: UIButton) {
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+        
+    }
 
 }
 
